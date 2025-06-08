c3fb124c15903344190f064d9b82f0a8f80e9441
MichielvanHattem <michielvanhattem1968@gmail.com>
2025-06-08 19:23:32 +0200

    Update Finny 4.5: definitieve .env configuratie en integraties (Azure, SharePoint, OpenAI)

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
 
 app.use(express.static('public'));
 
+app.get('/auth/login', (req, res) => {
+  res.redirect(process.env.AZURE_REDIRECT_URI);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server draait foutloos op poort ${PORT}`);

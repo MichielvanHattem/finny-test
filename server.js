e775264775996bbbee49b5b692d6e937e23bb85e
MichielvanHattem <michielvanhattem1968@gmail.com>
2025-06-08 18:26:31 +0200

    Herstel definitief: Toevoegen root (/) route en static files

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.use(express.json());
 
 app.use('/sp-files', authMiddleware, spFilesRoute);
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static('public'));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server draait foutloos op poort ${PORT}`);
